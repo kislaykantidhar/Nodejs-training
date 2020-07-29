7dbb7d69cb4f3f5f8f519385157afd4e1625ef08
Kislay Kanti Dhar <kislay.dhar@bitcanny.com>
2020-07-29 12:32:00 +0530

    first commit

---
+++ 01_Assignment.js
@@ -0,0 +1,17 @@
+//Print number 1 to 100 without using any loop. 
+
+function recursionprint(num)
+{
+    if (num==100)
+    {
+        console.log(num);
+        return;
+    }
+    else
+    {
+        console.log(num);
+        return recursionprint(++num);
+    }
+}
+
+recursionprint(1);
